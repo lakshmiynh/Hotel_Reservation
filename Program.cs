86e93b5f440340a583eab73fbf65b4db285edf51
Lakshmi <lakshmiynh8@gmail.com>
2024-04-04 08:50:43 +0530

    Added:add hotel in a hotel Reservation

--- Program.cs
+++ Program.cs
@@ -11,6 +11,16 @@ namespace HotelReservation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Hotel Reservation !");
+            Hotel[] hotels = new Hotel[3];
+
+            hotels[0] = new Hotel("Lakewood", 110);
+            hotels[1] = new Hotel("Bridgewood", 160);
+            hotels[2] = new Hotel("Ridgewood", 220);
+
+            foreach (Hotel hotel in hotels)
+            {
+                hotel.DisplayHotel();
+            }
         }
     }
 }
