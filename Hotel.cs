f641e4a3c69a4c78f41ff87efe9bb789881f3caa
Lakshmi <lakshmiynh8@gmail.com>
2024-04-04 08:48:19 +0530

    Added:add Hotel in a Hotel Resrvation

---
+++ Hotel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservation
+{
+    public  class Hotel
+    {
+        public string HotelName { get; set; }
+        public int RegularCustomerRate { get; set; }
+        public Hotel(string name, int rate)
+        {
+            HotelName = name;
+            RegularCustomerRate = rate;
+        }
+
+        public void DisplayHotel()
+        {
+            Console.WriteLine($"Hotel '{HotelName}' with regular customer rate ${RegularCustomerRate}.");
+        }
+    }
+}
